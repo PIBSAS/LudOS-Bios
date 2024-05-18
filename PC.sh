3594e3c2d1634d9685046922bdb895bb4b58b66f
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-05-18 12:29:41 -0300

    Update PC.sh

--- PC.sh
+++ PC.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "Removing all Bios, if are present"
+###########################################################################
+# Repositorio: LudOS-Bios 2024
+# Por: Raspberry Pi Buenos Aires ("https://sites.google.com/view/raspberrypibuenosaires/)
+# License: http://creativecommons.org/licenses/by-sa/4.0/
+###########################################################################
 RUTA=https://github.com/PIBSAS/LudOS-Bios/blob/main/
 RUTA_ARCH=https://archive.org/download/RaspberryPiBuenosAiresLudOSBios/
 echo
